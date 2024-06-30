81b3ad980c90bd4876a40ab23f89a1a91ac4560e
Mohammad Wasi <reallywasi@example.com>
2024-06-30 23:06:33 +0530

    categories listing page done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import variables from "../styles/variables.scss"
 import {Icon, IconButton } from "@mui/material"
 import SearchIcon from '@mui/icons-material/Search'
 // import Person from '@mui/icons-material/Person'
 // import Menu from '@mui/material/Menu'
-
+import {useSelector , useDispatch} from "react-redux"
 import { Search, Person } from "@mui/icons-material";
-
+import "../styles/Navbar.scss"
 import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from 'react-router-dom'
+import { setLogout } from '../redux/state'
+
 
 {/*================================================================================================== */}
-const Navbar = ({ user }) => {
+const Navbar = () => {
+
+const[dropdownMenu,setDropdownMenu]=useState(false);
+const user=useSelector((state)=>state.user)
+
+const dispatch=useDispatch()
+
   return (
     <div className='navbar'>
       <a href='/'>
@@ -21,25 +30,70 @@ const Navbar = ({ user }) => {
 {/*================================================================================================== */}
 
 
-      <div className='navbar-search'>
+      <div className='navbar_search'>
         <input type="text" placeholder='search...' />
         <IconButton>
           <SearchIcon sx={{ color: variables.pinkred }} />
         </IconButton>
       </div>
 {/*================================================================================================== */}
-      <div className='navbar-right'>
+      <div className='navbar_right'>
         {user ? (
-          <a href='/create-listing'>Become a Host</a>
+          <a href='/create-listing' className='host'>Become a Host</a>
         ) : (
-          <a href="/login">Become a HOST</a>
+          <a href="/login" className='host'>Become a HOST</a>
         )}
-        <button className='navbar-right-account'>
-        <MenuIcon sx={{ color: variables.darkgrey }} />     
-          
-        <Person sx={{ color: variables.darkgrey }} />
+        <button
+          className="navbar_right_account"
+          onClick={() => setDropdownMenu(!dropdownMenu)}
+        >
+          <MenuIcon sx={{ color: variables.darkgrey }} />
+          {!user ? (
+            <Person sx={{ color: variables.darkgrey }} />
+          ) : (
+            <img
+              src={`http://localhost:3001/${user.profileImagePath.replace(
+                "public",
+                ""
+              )}`}
+              alt="profile photo"
+              style={{ objectFit: "cover", borderRadius: "50%" }}
+            />
+          )}
+        </button>
 
-         </button>
+{/* ============================================================================================================ */}
+
+{
+  dropdownMenu && !user && ( <div className='navbar_right_accountmenu'> 
+  <Link to="/login">Login</Link>
+  <Link to="/register" >Sign Up</Link>
+  </div>
+  )
+}
+
+
+{/* ============================================================================================================ */}
+
+{dropdownMenu && user && (
+  <div className='navbar_right_accountmenu'>
+
+<Link to="">Trip List</Link>
+<Link to="">Wish List</Link>
+<Link to="">Property List</Link>
+<Link to="">Reservation List</Link>
+<Link to="">Become a host</Link>
+
+
+<Link to="/login" onClick={()=>{
+  dispatch(setLogout())
+}}>Logout</Link>
+
+  </div>
+)
+
+}
+
       </div>
     </div>
   )
