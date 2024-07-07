8a0632a7977f5fe75f20651024b25c77ff7886c0
Mohammad Wasi <reallywasi@example.com>
2024-07-07 13:31:22 +0530

    Listing page done with redux dev tool working

---
+++ client/src/components/ListingCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListingCard = () => {
+  return (
+    <div>
+      Listinf card
+    </div>
+  )
+}
+
+export default ListingCard
---
+++ client/src/components/Trial.js
@@ -0,0 +1,12 @@
+// so this'll be a explanatory file which'll explain about the whole project
+
+
+// so first I made sign up PAGE UI
+// it runs on localhost :3000
+
+// then I made backend routes and MongoDB connection 
+
+// then I made API routes 
+// then inside routes I made a file auth.js 
+
+// inside auth .js it contains all logic of Authentication 
