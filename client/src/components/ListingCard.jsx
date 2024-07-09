d510c051cebb4a85d7f75d1d001ef9965d409bdd
Mohammad Wasi <reallywasi@example.com>
2024-07-09 11:52:06 +0530

    Trip List page done with form and pages

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -7,7 +7,7 @@ import {
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { setWishList } from "../redux/state";
+import { setWishList } from "../redux/state";
 
 const ListingCard = ({
   listingId,
@@ -41,27 +41,7 @@ const ListingCard = ({
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  /* ADD TO WISHLIST */
-  const user = useSelector((state) => state.user);
-  const wishList = user?.wishList || [];
 
-  const isLiked = wishList?.find((item) => item?._id === listingId);
-
-  const patchWishList = async () => {
-    if (user?._id !== creator._id) {
-    const response = await fetch(
-      `http://localhost:3001/users/${user?._id}/${listingId}`,
-      {
-        method: "PATCH",
-        header: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const data = await response.json();
-    // dispatch(setWishList(data.wishList));
-  } else { return }
-  };
 
   return (
     <div
@@ -127,20 +107,6 @@ const ListingCard = ({
         </>
       )}
 
-      <button
-        className="favorite"
-        onClick={(e) => {
-          e.stopPropagation();
-          patchWishList();
-        }}
-        disabled={!user}
-      >
-        {isLiked ? (
-          <Favorite sx={{ color: "red" }} />
-        ) : (
-          <Favorite sx={{ color: "white" }} />
-        )}
-      </button>
     </div>
   );
 };
